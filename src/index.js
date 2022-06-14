5fec2b0460bc19f52f0278ef89eed546ddc5e995
richard <richard.ytli+f@gmail.com>
2022-06-14 12:01:03 +0800

    Process and log weather data

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import getWeatherData from './modules/api';
 
 const search = document.querySelector('#search-city');
 search.addEventListener('keydown', (e) => {
-  const {value} = search;
+  const { value } = search;
   if (e.code === 'Enter' && value.length > 0) {
     getWeatherData(value);
     search.value = '';
--- src/modules/api.js
+++ src/modules/api.js
@@ -1,13 +1,36 @@
-// https://api.openweathermap.org/data/2.5/weather?q={cityName}&units=metric&appid=463f90b73d25b0b65abf11031d0b03b9
+// https://api.openweathermap.org/data/2.5x/weather?q={cityName}&units=metric&appid=463f90b73d25b0b65abf11031d0b03b9
+
+function processData(input) {
+  const temperature = {
+    current: input.main.temp,
+    tempMin: input.main.temp_min,
+    tempMax: input.main.temp_max,
+    feelsLike: input.main.feels_like,
+  };
+  Object.keys(temperature).forEach((key) => {
+    temperature[key] = Math.round(temperature[key]);
+  });
+  const output = {
+    name: input.name,
+    country: input.sys.country,
+    weather: {
+      description: input.weather[0].description,
+      ...temperature,
+      humidity: input.main.humidity,
+    },
+  };
+  return output;
+}
 
 export default async function fetchData(query) {
   try {
-    const request = `https://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&appid=463f90b73d25b0b65abf11031d0b03b9`
-    const response = await fetch (request, {mode: 'cors'});
-    if (!response.status) throw new Error(response.statusText);
+    const request = `https://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&appid=463f90b73d25b0b65abf11031d0b03b9`;
+    const response = await fetch(request, { mode: 'cors' });
+    if (!response.ok) throw new Error(response.statusText);
     const data = await response.json();
-    console.log(data);
+    const output = processData(data);
+    console.log(output);
   } catch (error) {
-    console.error(error);
+    return console.error(error);
   }
 }
